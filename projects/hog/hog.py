eae612c1b4169c8bfc844af39f094230ecdfe297
Jarosław Kołosowski <jarokolosowski@gmail.com>
2024-02-05 10:23:10 +0100

    Project Hog - Problem 2

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -46,6 +46,10 @@ def boar_brawl(player_score, opponent_score):
     """
     # BEGIN PROBLEM 2
     "*** YOUR CODE HERE ***"
+    player_digit = player_score % 10
+    opponent_digit = (opponent_score // 10) % 10
+    difference = abs(opponent_digit - player_digit)
+    return max(1, 3 * difference)
     # END PROBLEM 2
 
 
