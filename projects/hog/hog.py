6fab0b918512821502420fbe22e6d27c2cce5b5f
Jarosław Kołosowski <jarokolosowski@gmail.com>
2024-02-05 13:43:14 +0100

    Project Hog - Problem 6

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -201,6 +201,9 @@ def always_roll(n):
     assert n >= 0 and n <= 10
     # BEGIN PROBLEM 6
     "*** YOUR CODE HERE ***"
+    def f(player_score, opponent_score):
+        return n
+    return f
     # END PROBLEM 6
 
 
