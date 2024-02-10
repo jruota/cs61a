a307965c121d95601378dfafaecf2fff1799c3ca
Jarosław Kołosowski <jarokolosowski@gmail.com>
2024-02-10 11:00:25 +0100

    Project Hog - Problem 12 (unfinished)
    I cannot make the final strategy have a higher chance of winning than the
    already existing strategies.

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -355,7 +355,10 @@ def final_strategy(score, opponent_score):
     *** YOUR DESCRIPTION HERE ***
     """
     # BEGIN PROBLEM 12
-    return 6  # Remove this line once implemented.
+    return boar_strategy(
+        score,
+        opponent_score,
+        threshold=sus_update(6, score, opponent_score))
     # END PROBLEM 12
 
 
