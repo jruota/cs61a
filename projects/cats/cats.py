b297c159f70f24973d4a56bf2d70deafa09264a0
Jarosław Kołosowski <jarokolosowski@gmail.com>
2024-02-22 14:47:37 +0100

    Project Cats - Problem 7

--- projects/cats/cats.py
+++ projects/cats/cats.py
@@ -232,23 +232,24 @@ def minimum_mewtations(typed, source, limit):
     >>> minimum_mewtations("ckiteus", "kittens", big_limit) # ckiteus -> kiteus -> kitteus -> kittens
     3
     """
-    assert False, 'Remove this line'
-    if ___________: # Base cases should go here, you may add more base cases as needed.
-        # BEGIN
-        "*** YOUR CODE HERE ***"
-        # END
+    # Base cases should go here, you may add more base cases as needed.
+    if limit < 0:
+        return 0
+    if typed == source:
+        return 0
+    if ((len(typed) == 0 and len(source) > 0) or
+        (len(typed) > 0 and len(source) == 0)):
+        return abs(len(typed) - len(source))
     # Recursive cases should go below here
-    if ___________: # Feel free to remove or add additional cases
-        # BEGIN
-        "*** YOUR CODE HERE ***"
-        # END
+    if typed[0] == source[0]:
+        return minimum_mewtations(typed[1:], source[1:], limit)
     else:
-        add = ... # Fill in these lines
-        remove = ...
-        substitute = ...
-        # BEGIN
-        "*** YOUR CODE HERE ***"
-        # END
+        add = 1 + minimum_mewtations(source[0] + typed, source, limit - 1)
+        remove = 1 + minimum_mewtations(typed[1:], source, limit - 1)
+        substitute = 1 + minimum_mewtations(source[0] + typed[1:],
+                                            source,
+                                            limit - 1)
+        return min(add, remove, substitute)
 
 
 def final_diff(typed, source, limit):
